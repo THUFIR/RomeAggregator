b5fa5db164e20cda69d2103ebe2e22987a4b6cc1
Thufir <hawat.thufir@gmail.com>
2014-11-03 14:49:03 -0800

    increment

--- src/dur/bounceme/net/rome/jpa/Link.java
+++ src/dur/bounceme/net/rome/jpa/Link.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dur.bounceme.net.rome.jpa;
 
 import java.io.Serializable;
@@ -21,10 +16,6 @@ import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author thufir
- */
 @Entity
 @Table(name = "links", catalog = "rome_aggregator", schema = "", uniqueConstraints = {
     @UniqueConstraint(columnNames = {"link"})})
